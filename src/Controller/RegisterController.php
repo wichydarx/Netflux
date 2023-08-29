d1b744b1414f4f36a2f76df89b5b6b551ce9f3d6
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-08-29 14:46:14 +0200

    Ajout directive lors de l'envoi du formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use App\Service\FileUploader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
 
+    private $passwordHash;
+    private $fileUploader;
 
-    public function __construct()
+    public function __construct(UserPasswordHasherInterface $passwordHash, FileUploader $fileUploader)
     {
+        $this->passwordHash = $passwordHash;
+        $this->fileUploader = $fileUploader;
     }
 
     #[Route('/register', name: 'app_register')]
@@ -24,7 +30,23 @@ class RegisterController extends AbstractController
         $form = $this->createForm(RegisterType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd('valide');
+
+            $passwordClear = $user->getPassword();
+            // Fonction pour hashé le mot de passe
+            $passwordIsHashed = $this->passwordHash->hashPassword($user, $passwordClear);
+            // Stockage du mot de passe hashé dans l'entité User
+            $user->setPassword($passwordIsHashed);
+
+            $avatar = $form->get('avatar')->getData();
+            // Si le champ avatar est rempli alors on utilise le service FileUploader afin d'upload l'image
+            if ($avatar) {
+                $newFilename = $this->fileUploader->upload($avatar);
+                $user->setAvatar($newFilename);
+            }
+
+            // Ajout des informations de l'entité User dans la base de données
+            $this->manager->persist($user);
+            $this->manager->flush();
         }
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
