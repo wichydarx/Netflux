0c53921e5efd3b54631dce62f4259f670530b516
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-08-29 15:09:27 +0200

    Création du service UploadFile

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use App\Service\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,11 +17,13 @@ class RegisterController extends AbstractController
 
     private $passwordHash;
     private $fileUploader;
+    private $manager;
 
-    public function __construct(UserPasswordHasherInterface $passwordHash, FileUploader $fileUploader)
+    public function __construct(UserPasswordHasherInterface $passwordHash, FileUploader $fileUploader, EntityManagerInterface $manager)
     {
         $this->passwordHash = $passwordHash;
         $this->fileUploader = $fileUploader;
+        $this->manager = $manager;
     }
 
     #[Route('/register', name: 'app_register')]
@@ -31,22 +34,26 @@ class RegisterController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $passwordClear = $user->getPassword();
-            // Fonction pour hashé le mot de passe
-            $passwordIsHashed = $this->passwordHash->hashPassword($user, $passwordClear);
-            // Stockage du mot de passe hashé dans l'entité User
-            $user->setPassword($passwordIsHashed);
+            try {
+                $passwordClear = $user->getPassword();
+                // Fonction pour hashé le mot de passe
+                $passwordIsHashed = $this->passwordHash->hashPassword($user, $passwordClear);
+                // Stockage du mot de passe hashé dans l'entité User
+                $user->setPassword($passwordIsHashed);
 
-            $avatar = $form->get('avatar')->getData();
-            // Si le champ avatar est rempli alors on utilise le service FileUploader afin d'upload l'image
-            if ($avatar) {
-                $newFilename = $this->fileUploader->upload($avatar);
-                $user->setAvatar($newFilename);
+                $avatar = $form->get('avatar')->getData();
+                // Si le champ avatar est rempli alors on utilise le service FileUploader afin d'upload l'image
+                if ($avatar) {
+                    $newFilename = $this->fileUploader->upload($avatar);
+                    $user->setAvatar($newFilename);
+                }
+
+                // Ajout des informations de l'entité User dans la base de données
+                $this->manager->persist($user);
+                $this->manager->flush();
+            } catch (\Throwable $th) {
+                $this->addFlash('danger', 'Une erreur est survenue lors de l\'ajout de la recette');
             }
-
-            // Ajout des informations de l'entité User dans la base de données
-            $this->manager->persist($user);
-            $this->manager->flush();
         }
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
