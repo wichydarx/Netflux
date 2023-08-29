8714ec57560369df4e61ecb34f22d184bc436e12
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-08-29 12:15:39 +0200

    Fonction construct (vide) et mise en place form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+
+
+    public function __construct()
     {
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function index(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd('valide');
+        }
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
+            'registerForm' => $form->createView()
         ]);
     }
 }
