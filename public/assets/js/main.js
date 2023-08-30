05b75ce5a6e824577939b2e5df138e4a3708c072
wichydarx <118377008+wichydarx@users.noreply.github.com>
2023-08-30 14:00:25 +0200

    rajout js carousel

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,29 @@
 const player = new Plyr("#player");
+
+
+const carousel = document.querySelector(".carouselItems");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+let currentIndex = 0;
+
+function showSlide(index) {
+    const items = carousel.querySelectorAll(".items");
+    items.forEach((item, i) => {
+        item.style.display = i >= index && i < index + 5 ? "block" : "none";
+    });
+}
+
+function nextSlide() {
+    currentIndex = Math.min(currentIndex + 1, carousel.children.length - 5);
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    showSlide(currentIndex);
+}
+
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+showSlide(currentIndex);
