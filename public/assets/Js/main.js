0e97cabe31c7f64e419f0f1ddb408887370cbb0b
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-08-30 11:56:23 +0200

    Modification Script carousel

--- public/assets/Js/main.js
+++ public/assets/Js/main.js
@@ -8,26 +8,18 @@ let currentIndex = 0;
 function showSlide(index) {
   const items = carousel.querySelectorAll(".items");
   items.forEach((item, i) => {
-    item.style.display = i >= index && i < index + 4 ? "block" : "none";
+    item.style.display = i >= index && i < index + 5 ? "block" : "none";
   });
-}
-
-function scrollToSlide(index) {
-  currentIndex = index;
-  showSlide(currentIndex);
-  carousel.style.transform = `translateX(-${
-    currentIndex * 20
-  }%)`; /* 20% pour 5 éléments */
 }
 
 function nextSlide() {
   currentIndex = Math.min(currentIndex + 1, carousel.children.length - 5);
-  scrollToSlide(currentIndex);
+  showSlide(currentIndex);
 }
 
 function prevSlide() {
   currentIndex = Math.max(currentIndex - 1, 0);
-  scrollToSlide(currentIndex);
+  showSlide(currentIndex);
 }
 
 prevButton.addEventListener("click", prevSlide);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
 
 class HomeController extends AbstractController
 {
@@ -28,5 +30,13 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
             'videos' => $videos
         ]);
+    }
+
+    #[Route('/movie', name: 'app_movie')]
+    public function test(Serializer $serializer)
+    {
+        $videos = $this->manager->getRepository(Video::class)->findAll();
+
+        return JsonResponse(json($videos));
     }
 }
