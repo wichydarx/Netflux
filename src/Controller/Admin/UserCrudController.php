42eb4693e0f196f1b8a347aa7d7047303bd70b0d
Marina VENG <marina.veng@gmail.com>
2023-08-30 11:46:46 +0200

    Modification BDD et EasyAdmin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -12,14 +16,22 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
+
+        $roles = [
+            'User' => 'ROLE_USER',
+            'Admin' => 'ROLE_ADMIN',
+        ];
+
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('firstName', 'Nom'),
+            TextField::new('lastName', 'Prenom'),
+            EmailField::new('email', 'Email'),
+            ChoiceField::new('roles', 'Roles')->setChoices($roles)->allowMultipleChoices(true),
         ];
     }
-    */
+    
 }
