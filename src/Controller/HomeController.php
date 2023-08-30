afe6d794b11568ddf979075d2e407764ac89c0f7
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-08-30 11:04:30 +0200

    Affichage vidéo carousel selon le genre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Video;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+
+        $videos = $this->manager->getRepository(Video::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'videos' => $videos
         ]);
     }
 }
