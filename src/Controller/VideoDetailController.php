72f5d1bd9d23344c77973f5606e297d6ba4ba135
Marina VENG <marina.veng@gmail.com>
2023-08-31 15:08:33 +0200

    Page video

--- src/Controller/VideoDetailController.php
+++ src/Controller/VideoDetailController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Video;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
