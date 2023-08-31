3fbbae236f50a3412664f2a04ec880fae87679b7
Siozi <103248052+Siozi@users.noreply.github.com>
2023-08-31 16:46:48 +0200

    push maudit

---
+++ src/Form/UpdateProfileType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UpdateProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('avatar', FileType::class, [
+                'data_class' => null,
+                'label' => 'Avatar',
+                'required' => false,
+            ])
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Modifier mon profil',
+                    'attr' =>
+                    [
+                        'class' => 'btn-warning'
+                    ]
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+            'data_class' => User::class,
+        ]);
+    }
+}
