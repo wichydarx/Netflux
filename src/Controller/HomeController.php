c19202292a911ec81db99e210bb5ad0ba16ff668
wichydarx <118377008+wichydarx@users.noreply.github.com>
2023-09-02 23:14:33 +0200

    notifications et reviews fini

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,48 +9,48 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
 
-    private $manager;
+  private $manager;
 
-    public function __construct(EntityManagerInterface $manager)
-    {
-        $this->manager = $manager;
-    }
+  public function __construct(EntityManagerInterface $manager)
+  {
+    $this->manager = $manager;
+  }
 
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+  #[Route('/', name: 'app_home')]
+  public function index(): Response
+  {
 
-        $videos = $this->manager->getRepository(Video::class)->findAll();
-        $genres = $this->manager->getRepository(Genre::class)->findAll();
+    $videos = $this->manager->getRepository(Video::class)->findAll();
+    $genres = $this->manager->getRepository(Genre::class)->findAll();
 
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'videos' => $videos,
-            'genres' => $genres
-        ]);
-    }
+    return $this->render('home/index.html.twig', [
+      'controller_name' => 'HomeController',
+      'videos' => $videos,
+      'genres' => $genres
+    ]);
+  }
 
-    #[Route('/search', name: 'app_search')]
-    public function search(Request $request, EntityManagerInterface $em): Response
+  #[Route('/search', name: 'app_search')]
+  public function search(Request $request, EntityManagerInterface $em): Response
   {
     $title = $request->request->get('search');
     $videos = $em->getRepository(Video::class)->searchByTitle($title);
-    
+
     return $this->render('home/search.html.twig', [
       'videos' => $videos,
       'title' => $title
     ]);
   }
-    #[Route('/api/videos', name: 'app_movie')]
-    public function test(VideoRepository $videoRepository)
-    {
+  #[Route('/api/videos', name: 'app_movie')]
+  public function test(VideoRepository $videoRepository)
+  {
 
-        return $this->json($videoRepository->findAll(), 200, [], []);
-    }
-
+    return $this->json($videoRepository->findAll(), 200, [], []);
+  }
 }
